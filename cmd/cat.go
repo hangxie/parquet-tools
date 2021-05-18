766eac486303364adceb3180f797bdabbaf5a803
Hang Xie <7977860+hangxie@users.noreply.github.com>
2021-05-18 14:57:18 -0700

    add cat command

--- cmd/cat.go
+++ cmd/cat.go
@@ -1,17 +1,76 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 // CatCmd is a kong command for cat
 type CatCmd struct {
 	CommonOption
-	JSON bool `help:"Output JSON format."`
+	Limit    int64   `short:"l" help:"Max number of rows to output." default:"-1"`
+	PageSize int     `short:"p" help:"Pagination size to read from Parquet." default:"1000"`
+	Sampling float64 `short:"s" help:"Sampling percentage." default:"1.0"`
 }
 
 // Run does actual cat job
 func (c *CatCmd) Run(ctx *Context) error {
-	fmt.Println("cat", c.URI)
+	if c.Limit == -1 {
+		c.Limit = int64(1<<63 - 1)
+	}
+	if c.Limit <= 0 {
+		return fmt.Errorf("invalid limit %d, needs to be a positive number (-1 for unlimited)", c.Limit)
+	}
+	if c.PageSize < 1 {
+		return fmt.Errorf("invalid page size %d, needs to be at least 1", c.PageSize)
+	}
+	// note that sampling rate at 0.0 is allowed, while it does not output anything
+	if c.Sampling < 0.0 || c.Sampling > 1.0 {
+		return fmt.Errorf("invalid sampling %f, needs to be between 0.0 and 1.0", c.Sampling)
+	}
+
+	reader, err := newParquetFileReader(c.URI)
+	if err != nil {
+		return err
+	}
+	defer reader.PFile.Close()
+
+	// Output rows one by one to avoid running out of memory with a jumbo list
+	fmt.Print("[")
+	rand.Seed(time.Now().UnixNano())
+	firstItem := true
+	counter := int64(0)
+	for counter < c.Limit {
+		rows, err := reader.ReadByNumber(c.PageSize)
+		if err != nil {
+			return fmt.Errorf("failed to cat: %s", err)
+		}
+		if len(rows) == 0 {
+			break
+		}
+
+		for _, row := range rows {
+			if rand.Float64() >= c.Sampling {
+				continue
+			}
+			if firstItem {
+				firstItem = false
+			} else {
+				fmt.Print(",")
+			}
+
+			buf, _ := json.Marshal(row)
+			fmt.Print(string(buf))
+
+			counter += 1
+			if counter >= c.Limit {
+				break
+			}
+		}
+	}
+	fmt.Println("]")
+
 	return nil
 }
---
+++ cmd/cat_test.go
@@ -0,0 +1,150 @@
+package cmd
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_CatCmd_Run_non_existent_file(t *testing.T) {
+	cmd := &CatCmd{
+		Limit:    10,
+		PageSize: 10,
+		Sampling: 1.0,
+		CommonOption: CommonOption{
+			URI: "file/does/not/exist",
+		},
+	}
+
+	err := cmd.Run(&Context{})
+	assert.NotNil(t, err)
+	assert.Contains(t, err.Error(), "failed to open local")
+}
+
+func Test_CatCmd_Run_invalid_limit(t *testing.T) {
+	cmd := &CatCmd{
+		Limit:    -10,
+		PageSize: 10,
+		Sampling: 0.5,
+		CommonOption: CommonOption{
+			URI: "testdata/all-types.parquet",
+		},
+	}
+
+	err := cmd.Run(&Context{})
+	assert.NotNil(t, err)
+	assert.Contains(t, err.Error(), "invalid limit")
+}
+
+func Test_CatCmd_Run_default_limit(t *testing.T) {
+	cmd := &CatCmd{
+		Limit:    -1,
+		PageSize: 10,
+		Sampling: 0.5,
+		CommonOption: CommonOption{
+			URI: "testdata/all-types.parquet",
+		},
+	}
+
+	err := cmd.Run(&Context{})
+	assert.Nil(t, err)
+	assert.Equal(t, cmd.Limit, int64(1<<63-1))
+}
+
+func Test_CatCmd_Run_invalid_page_size(t *testing.T) {
+	cmd := &CatCmd{
+		Limit:    10,
+		PageSize: 0,
+		Sampling: 0.5,
+		CommonOption: CommonOption{
+			URI: "testdata/all-types.parquet",
+		},
+	}
+
+	err := cmd.Run(&Context{})
+	assert.NotNil(t, err)
+	assert.Contains(t, err.Error(), "invalid page size")
+}
+
+func Test_CatCmd_Run_invalid_sampling_too_big(t *testing.T) {
+	cmd := &CatCmd{
+		Limit:    10,
+		PageSize: 10,
+		Sampling: 2.0,
+		CommonOption: CommonOption{
+			URI: "testdata/all-types.parquet",
+		},
+	}
+
+	err := cmd.Run(&Context{})
+	assert.NotNil(t, err)
+	assert.Contains(t, err.Error(), "invalid sampling")
+}
+
+func Test_CatCmd_Run_invalid_sampling_too_small(t *testing.T) {
+	cmd := &CatCmd{
+		Limit:    10,
+		PageSize: 10,
+		Sampling: -0.5,
+		CommonOption: CommonOption{
+			URI: "testdata/all-types.parquet",
+		},
+	}
+
+	err := cmd.Run(&Context{})
+	assert.NotNil(t, err)
+	assert.Contains(t, err.Error(), "invalid sampling")
+}
+
+func Test_CatCmd_Run_good_default(t *testing.T) {
+	cmd := &CatCmd{
+		Limit:    10,
+		PageSize: 10,
+		Sampling: 1.0,
+		CommonOption: CommonOption{
+			URI: "testdata/good.parquet",
+		},
+	}
+
+	stdout, stderr := captureStdoutStderr(func() {
+		assert.Nil(t, cmd.Run(&Context{}))
+	})
+	assert.Equal(t, stdout, `[{"Shoe_brand":"shoe_brand","Shoe_name":"shoe_name"},{"Shoe_brand":"nike","Shoe_name":"air_griffey"},{"Shoe_brand":"fila","Shoe_name":"grant_hill_2"},{"Shoe_brand":"steph_curry","Shoe_name":"curry7"}]`+
+		"\n")
+	assert.Equal(t, stderr, "")
+}
+
+func Test_CatCmd_Run_good_limit(t *testing.T) {
+	cmd := &CatCmd{
+		Limit:    2,
+		PageSize: 10,
+		Sampling: 1.0,
+		CommonOption: CommonOption{
+			URI: "testdata/good.parquet",
+		},
+	}
+
+	stdout, stderr := captureStdoutStderr(func() {
+		assert.Nil(t, cmd.Run(&Context{}))
+	})
+	assert.Equal(t, stdout, `[{"Shoe_brand":"shoe_brand","Shoe_name":"shoe_name"},{"Shoe_brand":"nike","Shoe_name":"air_griffey"}]`+
+		"\n")
+	assert.Equal(t, stderr, "")
+}
+
+func Test_CatCmd_Run_good_sampling(t *testing.T) {
+	cmd := &CatCmd{
+		Limit:    2,
+		PageSize: 10,
+		Sampling: 0.0,
+		CommonOption: CommonOption{
+			URI: "testdata/good.parquet",
+		},
+	}
+
+	stdout, stderr := captureStdoutStderr(func() {
+		assert.Nil(t, cmd.Run(&Context{}))
+	})
+	assert.Equal(t, stdout, "[]\n")
+	assert.Equal(t, stderr, "")
+}
