84d5e07b689c7bd09ce9f2640f1feb3012e2e644
Hang Xie <7977860+hangxie@users.noreply.github.com>
2021-05-20 22:17:58 -0700

    tweak version default output to meet common sense

--- cmd/version.go
+++ cmd/version.go
@@ -7,24 +7,32 @@ import (
 
 // VersionCmd is a kong command for version
 type VersionCmd struct {
-	JSON bool `short:"j" help:"Output in JSON format." default:"false"`
+	JSON      bool `short:"j" help:"Output in JSON format." default:"false"`
+	BuildTime bool `short:"b" help:"Output build time as well." default:"false"`
 }
 
 // Run does actual version job
 func (c *VersionCmd) Run(ctx *Context) error {
-	if c.JSON {
-		v := struct {
-			Version   string
-			BuildTime string
-		}{
-			Version:   ctx.Version,
-			BuildTime: ctx.Build,
+	if !c.JSON {
+		fmt.Println(ctx.Version)
+		if c.BuildTime {
+			fmt.Println(ctx.Build)
 		}
-		buf, _ := json.Marshal(v)
-		fmt.Println(string(buf))
-	} else {
-		fmt.Println("Version:", ctx.Version)
-		fmt.Println("Build Time:", ctx.Build)
+		return nil
 	}
+
+	v := struct {
+		Version   string
+		BuildTime *string `json:",omitempty"`
+	}{
+		Version:   ctx.Version,
+		BuildTime: nil,
+	}
+	if c.BuildTime {
+		v.BuildTime = &ctx.Build
+	}
+	buf, _ := json.Marshal(v)
+	fmt.Println(string(buf))
+
 	return nil
 }
