6ddf7216ae9b74ca4f33a809d68b602366d76ea9
Hang Xie <7977860+hangxie@users.noreply.github.com>
2021-11-09 06:17:27 -0800

    refactor build facilities

---
+++ .circleci/gen-meta.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -eou pipefail
+
+(cd ${BUILDDIR}/release; \
+    sha512sum parquet-tools* > checksum-sha512.txt; \
+    md5sum parquet-tools* > checksum-md5.txt)
+
+# version file
+echo ${VERSION} > ${BUILDDIR}/VERSION
+PREV_VERSION=$(git tag --sort=-committerdate | head -2 | tail -1)
+
+# changelog file
+echo "Changes since [${PREV_VERSION}](https://github.com/hangxie/parquet-tools/releases/tag/${PREV_VERSION}):" > ${BUILDDIR}/CHANGELOG
+echo >> ${BUILDDIR}/CHANGELOG
+git log --pretty=format:"* %h %s" ${VERSION}...${PREV_VERSION} >> ${BUILDDIR}/CHANGELOG
+echo >> ${BUILDDIR}/CHANGELOG
+
+# license file
+cp LICENSE ${BUILDDIR}/release/LICENSE
