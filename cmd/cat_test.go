0d2dcf9be7ecfe5ca6ee1f8a682c5b729785f36a
Hang Xie <7977860+hangxie@users.noreply.github.com>
2021-12-29 07:26:10 -0800

    add dedicated test data for decimal

--- cmd/cat_test.go
+++ cmd/cat_test.go
@@ -277,7 +277,7 @@ func Test_CatCmd_Run_good_decimal_zero(t *testing.T) {
 		PageSize:    10,
 		SampleRatio: 1.0,
 		CommonOption: CommonOption{
-			URI: "testdata/all-types.parquet",
+			URI: "testdata/decimals.parquet",
 		},
 		Format: "jsonl",
 	}
@@ -285,7 +285,7 @@ func Test_CatCmd_Run_good_decimal_zero(t *testing.T) {
 	stdout, stderr := captureStdoutStderr(func() {
 		assert.Nil(t, cmd.Run(&Context{}))
 	})
-	assert.Equal(t, stdout, `{"Bool":true,"Int32":0,"Int64":0,"Int96":"90\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000","Float":0,"Double":0,"Bytearray":"ByteArray","FixedLenByteArray":"HelloWorld","Utf8":"utf8","Int_8":0,"Int_16":0,"Int_32":0,"Int_64":0,"Uint_8":0,"Uint_16":0,"Uint_32":0,"Uint_64":0,"Date":0,"Date2":0,"Timemillis":0,"Timemillis2":0,"Timemicros":0,"Timemicros2":0,"Timestampmillis":0,"Timestampmillis2":0,"Timestampmicros":0,"Timestampmicros2":0,"Interval":"90\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000","Decimal1":123.45,"Decimal2":123.45,"Decimal3":0,"Decimal4":0,"Decimal5":0,"Decimal_pointer":null,"Map":{"One":1,"Two":2},"List":["item1","item2"],"Repeated":[1,2,3],"NestedMap":{},"NestedList":[]}`+"\n")
+	assert.Equal(t, stdout, `{"Decimal1":0,"Decimal2":0,"Decimal3":0,"Decimal4":0,"Decimal_pointer":null}`+"\n")
 	assert.Equal(t, stderr, "")
 }
 
@@ -296,7 +296,7 @@ func Test_CatCmd_Run_good_decimal_fraction(t *testing.T) {
 		PageSize:    10,
 		SampleRatio: 1.0,
 		CommonOption: CommonOption{
-			URI: "testdata/all-types.parquet",
+			URI: "testdata/decimals.parquet",
 		},
 		Format: "jsonl",
 	}
@@ -304,7 +304,7 @@ func Test_CatCmd_Run_good_decimal_fraction(t *testing.T) {
 	stdout, stderr := captureStdoutStderr(func() {
 		assert.Nil(t, cmd.Run(&Context{}))
 	})
-	assert.Equal(t, stdout, `{"Bool":false,"Int32":1,"Int64":1,"Int96":"90\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000","Float":0.5,"Double":0.5,"Bytearray":"ByteArray","FixedLenByteArray":"HelloWorld","Utf8":"utf8","Int_8":1,"Int_16":1,"Int_32":1,"Int_64":1,"Uint_8":1,"Uint_16":1,"Uint_32":1,"Uint_64":1,"Date":1,"Date2":0,"Timemillis":1,"Timemillis2":0,"Timemicros":1,"Timemicros2":0,"Timestampmillis":1,"Timestampmillis2":0,"Timestampmicros":1,"Timestampmicros2":0,"Interval":"90\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000","Decimal1":123.45,"Decimal2":123.45,"Decimal3":0.11,"Decimal4":0.11,"Decimal5":0,"Decimal_pointer":0.11,"Map":{"One":1,"Two":2},"List":["item1","item2"],"Repeated":[1,2,3],"NestedMap":{},"NestedList":[]}`+"\n")
+	assert.Equal(t, stdout, `{"Decimal1":0.11,"Decimal2":0.11,"Decimal3":0.11,"Decimal4":0.11,"Decimal_pointer":0.11}`+"\n")
 	assert.Equal(t, stderr, "")
 }
 
@@ -315,7 +315,7 @@ func Test_CatCmd_Run_good_decimal_normal(t *testing.T) {
 		PageSize:    10,
 		SampleRatio: 1.0,
 		CommonOption: CommonOption{
-			URI: "testdata/all-types.parquet",
+			URI: "testdata/decimals.parquet",
 		},
 		Format: "jsonl",
 	}
@@ -323,7 +323,7 @@ func Test_CatCmd_Run_good_decimal_normal(t *testing.T) {
 	stdout, stderr := captureStdoutStderr(func() {
 		assert.Nil(t, cmd.Run(&Context{}))
 	})
-	assert.Equal(t, stdout, `{"Bool":true,"Int32":2,"Int64":2,"Int96":"90\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000","Float":1,"Double":1,"Bytearray":"ByteArray","FixedLenByteArray":"HelloWorld","Utf8":"utf8","Int_8":2,"Int_16":2,"Int_32":2,"Int_64":2,"Uint_8":2,"Uint_16":2,"Uint_32":2,"Uint_64":2,"Date":2,"Date2":0,"Timemillis":2,"Timemillis2":0,"Timemicros":2,"Timemicros2":0,"Timestampmillis":2,"Timestampmillis2":0,"Timestampmicros":2,"Timestampmicros2":0,"Interval":"90\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000","Decimal1":123.45,"Decimal2":123.45,"Decimal3":2.22,"Decimal4":2.22,"Decimal5":0,"Decimal_pointer":2.22,"Map":{"One":1,"Two":2},"List":["item1","item2"],"Repeated":[1,2,3],"NestedMap":{},"NestedList":[]}`+"\n")
+	assert.Equal(t, stdout, `{"Decimal1":2.22,"Decimal2":2.22,"Decimal3":2.22,"Decimal4":2.22,"Decimal_pointer":2.22}`+"\n")
 	assert.Equal(t, stderr, "")
 }
 
@@ -334,7 +334,7 @@ func Test_CatCmd_Run_good_decimal_negative_zero(t *testing.T) {
 		PageSize:    10,
 		SampleRatio: 1.0,
 		CommonOption: CommonOption{
-			URI: "testdata/all-types.parquet",
+			URI: "testdata/decimals.parquet",
 		},
 		Format: "jsonl",
 	}
@@ -342,7 +342,7 @@ func Test_CatCmd_Run_good_decimal_negative_zero(t *testing.T) {
 	stdout, stderr := captureStdoutStderr(func() {
 		assert.Nil(t, cmd.Run(&Context{}))
 	})
-	assert.Equal(t, stdout, `{"Bool":false,"Int32":3,"Int64":3,"Int96":"90\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000","Float":1.5,"Double":1.5,"Bytearray":"ByteArray","FixedLenByteArray":"HelloWorld","Utf8":"utf8","Int_8":3,"Int_16":3,"Int_32":3,"Int_64":3,"Uint_8":3,"Uint_16":3,"Uint_32":3,"Uint_64":3,"Date":3,"Date2":0,"Timemillis":3,"Timemillis2":0,"Timemicros":3,"Timemicros2":0,"Timestampmillis":3,"Timestampmillis2":0,"Timestampmicros":3,"Timestampmicros2":0,"Interval":"90\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000","Decimal1":123.45,"Decimal2":123.45,"Decimal3":0,"Decimal4":0,"Decimal5":0,"Decimal_pointer":null,"Map":{"One":1,"Two":2},"List":["item1","item2"],"Repeated":[1,2,3],"NestedMap":{},"NestedList":[]}`+"\n")
+	assert.Equal(t, stdout, `{"Decimal1":0,"Decimal2":0,"Decimal3":0,"Decimal4":0,"Decimal_pointer":null}`+"\n")
 	assert.Equal(t, stderr, "")
 }
 
@@ -353,7 +353,7 @@ func Test_CatCmd_Run_good_decimal_negative_fraction(t *testing.T) {
 		PageSize:    10,
 		SampleRatio: 1.0,
 		CommonOption: CommonOption{
-			URI: "testdata/all-types.parquet",
+			URI: "testdata/decimals.parquet",
 		},
 		Format: "jsonl",
 	}
@@ -361,7 +361,7 @@ func Test_CatCmd_Run_good_decimal_negative_fraction(t *testing.T) {
 	stdout, stderr := captureStdoutStderr(func() {
 		assert.Nil(t, cmd.Run(&Context{}))
 	})
-	assert.Equal(t, stdout, `{"Bool":true,"Int32":4,"Int64":4,"Int96":"90\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000","Float":2,"Double":2,"Bytearray":"ByteArray","FixedLenByteArray":"HelloWorld","Utf8":"utf8","Int_8":4,"Int_16":4,"Int_32":4,"Int_64":4,"Uint_8":4,"Uint_16":4,"Uint_32":4,"Uint_64":4,"Date":4,"Date2":0,"Timemillis":4,"Timemillis2":0,"Timemicros":4,"Timemicros2":0,"Timestampmillis":4,"Timestampmillis2":0,"Timestampmicros":4,"Timestampmicros2":0,"Interval":"90\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000","Decimal1":123.45,"Decimal2":123.45,"Decimal3":-0.11,"Decimal4":-0.11,"Decimal5":0,"Decimal_pointer":-0.11,"Map":{"One":1,"Two":2},"List":["item1","item2"],"Repeated":[1,2,3],"NestedMap":{},"NestedList":[]}`+"\n")
+	assert.Equal(t, stdout, `{"Decimal1":-0.11,"Decimal2":-0.11,"Decimal3":-0.11,"Decimal4":-0.11,"Decimal_pointer":-0.11}`+"\n")
 	assert.Equal(t, stderr, "")
 }
 
@@ -372,7 +372,7 @@ func Test_CatCmd_Run_good_decimal_negative_normal(t *testing.T) {
 		PageSize:    10,
 		SampleRatio: 1.0,
 		CommonOption: CommonOption{
-			URI: "testdata/all-types.parquet",
+			URI: "testdata/decimals.parquet",
 		},
 		Format: "jsonl",
 	}
@@ -380,6 +380,6 @@ func Test_CatCmd_Run_good_decimal_negative_normal(t *testing.T) {
 	stdout, stderr := captureStdoutStderr(func() {
 		assert.Nil(t, cmd.Run(&Context{}))
 	})
-	assert.Equal(t, stdout, `{"Bool":false,"Int32":5,"Int64":5,"Int96":"90\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000","Float":2.5,"Double":2.5,"Bytearray":"ByteArray","FixedLenByteArray":"HelloWorld","Utf8":"utf8","Int_8":5,"Int_16":5,"Int_32":5,"Int_64":5,"Uint_8":5,"Uint_16":5,"Uint_32":5,"Uint_64":5,"Date":5,"Date2":0,"Timemillis":5,"Timemillis2":0,"Timemicros":5,"Timemicros2":0,"Timestampmillis":5,"Timestampmillis2":0,"Timestampmicros":5,"Timestampmicros2":0,"Interval":"90\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000","Decimal1":123.45,"Decimal2":123.45,"Decimal3":-2.22,"Decimal4":-2.22,"Decimal5":0,"Decimal_pointer":-2.22,"Map":{"One":1,"Two":2},"List":["item1","item2"],"Repeated":[1,2,3],"NestedMap":{},"NestedList":[]}`+"\n")
+	assert.Equal(t, stdout, `{"Decimal1":-2.22,"Decimal2":-2.22,"Decimal3":-2.22,"Decimal4":-2.22,"Decimal_pointer":-2.22}`+"\n")
 	assert.Equal(t, stderr, "")
 }
