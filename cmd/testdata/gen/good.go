7674beddc9348f19b3b8b9764da9ace9e9ef2fcc
Hang Xie <7977860+hangxie@users.noreply.github.com>
2023-03-19 04:21:37 -0700

    add code to generate good.parquet

---
+++ cmd/testdata/gen/good.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/xitongsys/parquet-go-source/local"
+	"github.com/xitongsys/parquet-go/writer"
+)
+
+type Shoe struct {
+	ShoeBrand string `parquet:"name=shoe_brand, type=BYTE_ARRAY, convertedtype=UTF8"`
+	ShoeName  string `parquet:"name=shoe_name, type=BYTE_ARRAY, convertedtype=UTF8"`
+}
+
+func main() {
+	fw, err := local.NewLocalFileWriter("good.parquet")
+	if err != nil {
+		fmt.Println("Can't create local file", err)
+		return
+	}
+
+	pw, err := writer.NewParquetWriter(fw, new(Shoe), 4)
+	if err != nil {
+		fmt.Println("Can't create parquet writer", err)
+		return
+	}
+
+	pw.Write(Shoe{"nike", "air_griffey"})
+	pw.Write(Shoe{"fila", "grant_hill_2"})
+	pw.Write(Shoe{"steph_curry", "curry7"})
+	if err = pw.WriteStop(); err != nil {
+		fmt.Println("WriteStop error", err)
+		return
+	}
+	fw.Close()
+}
