37b4e974ccff2e5b155f58bbdb330c11cde92c78
Hang Xie <7977860+hangxie@users.noreply.github.com>
2024-06-20 23:29:37 -0700

    add CSV format to schema command

--- cmd/schema.go
+++ cmd/schema.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 
 	"github.com/hangxie/parquet-tools/internal"
 )
@@ -11,12 +12,13 @@ var (
 	formatRaw  string = "raw"
 	formatJSON string = "json"
 	formatGo   string = "go"
+	formatCSV  string = "csv"
 )
 
 // SchemaCmd is a kong command for schema
 type SchemaCmd struct {
 	internal.ReadOption
-	Format string `short:"f" help:"Schema format (raw/json/go)." enum:"raw,json,go" default:"json"`
+	Format string `short:"f" help:"Schema format (raw/json/go/csv)." enum:"raw,json,go,csv" default:"json"`
 }
 
 // Run does actual schema job
@@ -42,6 +44,22 @@ func (c SchemaCmd) Run() error {
 			return err
 		}
 		fmt.Printf("type %s %s\n", schemaRoot.Name, snippet)
+	case formatCSV:
+		jsonSchema := internal.NewJSONSchemaNode(*schemaRoot).Schema()
+		s := make([]string, len(jsonSchema.Fields))
+		for i, f := range jsonSchema.Fields {
+			if len(f.Fields) != 0 {
+				return fmt.Errorf("CSV supports flat schema only")
+			}
+			if strings.Contains(f.Tag, "repetitiontype=REPEATED") {
+				return fmt.Errorf("CSV does not support column in LIST type")
+			}
+			if strings.Contains(f.Tag, "repetitiontype=OPTIONAL") {
+				return fmt.Errorf("CSV does not support optional column")
+			}
+			s[i] = strings.Replace(f.Tag, ", repetitiontype=REQUIRED", "", 1)
+		}
+		fmt.Println(strings.Join(s, "\n"))
 	default:
 		return fmt.Errorf("unknown schema format [%s]", c.Format)
 	}
---
+++ testdata/gen/csv/main.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/xitongsys/parquet-go-source/local"
+	"github.com/xitongsys/parquet-go/parquet"
+	"github.com/xitongsys/parquet-go/writer"
+)
+
+func main() {
+	good()
+	optional()
+	repeated()
+	nested()
+}
+
+func good() {
+	type Student struct {
+		Id          int64   `parquet:"name=Id, type=INT64"`
+		Name        string  `parquet:"name=Name, type=BYTE_ARRAY, convertedtype=UTF8"`
+		Age         int32   `parquet:"name=Age, type=INT32"`
+		Temperature float32 `parquet:"name=Temperature, type=FLOAT"`
+		Vaccinated  bool    `parquet:"name=Vaccinated, type=BOOLEAN"`
+	}
+
+	fw, err := local.NewLocalFileWriter("csv-good.parquet")
+	if err != nil {
+		fmt.Println("Can't create local file", err)
+		return
+	}
+
+	pw, err := writer.NewParquetWriter(fw, new(Student), 4)
+	if err != nil {
+		fmt.Println("Can't create parquet writer", err)
+		return
+	}
+
+	pw.CompressionType = parquet.CompressionCodec_GZIP
+	pw.Write(Student{123, "John Doe", 30, 98.2, false})
+	pw.Write(Student{123, "Jane Doe", 25, 98.7, true})
+	if err = pw.WriteStop(); err != nil {
+		fmt.Println("WriteStop error", err)
+		return
+	}
+	fw.Close()
+}
+
+func optional() {
+	type Student struct {
+		Id          int64    `parquet:"name=Id, type=INT64"`
+		Name        string   `parquet:"name=Name, type=BYTE_ARRAY, convertedtype=UTF8"`
+		Age         int32    `parquet:"name=Age, type=INT32"`
+		Temperature *float32 `parquet:"name=Temperature, type=FLOAT, repetitiontype=OPTIONAL"`
+		Vaccinated  bool     `parquet:"name=Vaccinated, type=BOOLEAN"`
+	}
+
+	fw, err := local.NewLocalFileWriter("csv-optional.parquet")
+	if err != nil {
+		fmt.Println("Can't create local file", err)
+		return
+	}
+
+	pw, err := writer.NewParquetWriter(fw, new(Student), 4)
+	if err != nil {
+		fmt.Println("Can't create parquet writer", err)
+		return
+	}
+
+	pw.CompressionType = parquet.CompressionCodec_GZIP
+	pw.Write(Student{123, "John Doe", 30, nil, false})
+	pw.Write(Student{123, "Jane Doe", 25, nil, true})
+	if err = pw.WriteStop(); err != nil {
+		fmt.Println("WriteStop error", err)
+		return
+	}
+	fw.Close()
+}
+
+func repeated() {
+	type Student struct {
+		Id          int64     `parquet:"name=Id, type=INT64"`
+		Name        string    `parquet:"name=Name, type=BYTE_ARRAY, convertedtype=UTF8"`
+		Age         int32     `parquet:"name=Age, type=INT32"`
+		Temperature []float32 `parquet:"name=Temperature, type=FLOAT, repetitiontype=REPEATED"`
+		Vaccinated  bool      `parquet:"name=Vaccinated, type=BOOLEAN"`
+	}
+
+	fw, err := local.NewLocalFileWriter("csv-repeated.parquet")
+	if err != nil {
+		fmt.Println("Can't create local file", err)
+		return
+	}
+
+	pw, err := writer.NewParquetWriter(fw, new(Student), 4)
+	if err != nil {
+		fmt.Println("Can't create parquet writer", err)
+		return
+	}
+
+	pw.CompressionType = parquet.CompressionCodec_GZIP
+	pw.Write(Student{123, "John Doe", 30, []float32{}, false})
+	pw.Write(Student{123, "Jane Doe", 25, []float32{98.1, 99.2}, true})
+	if err = pw.WriteStop(); err != nil {
+		fmt.Println("WriteStop error", err)
+		return
+	}
+	fw.Close()
+}
+
+func nested() {
+	type Student struct {
+		Id          int64     `parquet:"name=Id, type=INT64"`
+		Name        string    `parquet:"name=Name, type=BYTE_ARRAY, convertedtype=UTF8"`
+		Age         int32     `parquet:"name=Age, type=INT32"`
+		Temperature []float32 `parquet:"name=Temperature, type=LIST, valuetype=FLOAT"`
+		Vaccinated  bool      `parquet:"name=Vaccinated, type=BOOLEAN"`
+	}
+
+	fw, err := local.NewLocalFileWriter("csv-nested.parquet")
+	if err != nil {
+		fmt.Println("Can't create local file", err)
+		return
+	}
+
+	pw, err := writer.NewParquetWriter(fw, new(Student), 4)
+	if err != nil {
+		fmt.Println("Can't create parquet writer", err)
+		return
+	}
+
+	pw.CompressionType = parquet.CompressionCodec_GZIP
+	pw.Write(Student{123, "John Doe", 30, []float32{}, false})
+	pw.Write(Student{123, "Jane Doe", 25, []float32{98.4, 99.3}, true})
+	if err = pw.WriteStop(); err != nil {
+		fmt.Println("WriteStop error", err)
+		return
+	}
+	fw.Close()
+}
