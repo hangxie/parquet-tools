15941f4010076fdbcae95cbcf267c1f1e99f2c91
Hang Xie <7977860+hangxie@users.noreply.github.com>
2024-09-27 07:32:47 -0700

    replace links to USAGE with README

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func main() {
 		&cli,
 		kong.UsageOnError(),
 		kong.ConfigureHelp(kong.HelpOptions{Compact: true}),
-		kong.Description("Utility inspect Parquet files, for full usage see https://github.com/hangxie/parquet-tools/blob/main/USAGE.md"),
+		kong.Description("Utility inspect Parquet files, for full usage see https://github.com/hangxie/parquet-tools/blob/main/README.md#Installation-and-Usage-of-parquet-tools"),
 	)
 	kongplete.Complete(parser, kongplete.WithPredictor("file", complete.PredictFiles("*")))
 
