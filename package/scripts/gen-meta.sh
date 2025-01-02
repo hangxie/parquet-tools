eb58c74d7d50184ee534665f1d68bf5d6f174bc5
Hang Xie <7977860+hangxie@users.noreply.github.com>
2025-01-02 13:58:12 -0800

    minor changes to build CHANGELOG

--- package/scripts/gen-meta.sh
+++ package/scripts/gen-meta.sh
@@ -7,13 +7,15 @@ set -euo pipefail
 
 # version file
 echo ${VERSION} > ${BUILD_DIR}/VERSION
-PREV_VERSION=$(git tag --sort=-committerdate | head -2 | tail -1)
+PREV_VERSION=$(git describe --abbrev=0 --tags ${VERSION}^)
 
 # changelog file
-echo "Changes since [${PREV_VERSION}](https://github.com/hangxie/parquet-tools/releases/tag/${PREV_VERSION}):" > ${BUILD_DIR}/CHANGELOG
-echo >> ${BUILD_DIR}/CHANGELOG
-git log --pretty=format:"* %h %s" ${VERSION}...${PREV_VERSION} >> ${BUILD_DIR}/CHANGELOG
-echo >> ${BUILD_DIR}/CHANGELOG
+(
+    echo "Changes since [${PREV_VERSION}](https://github.com/hangxie/parquet-tools/releases/tag/${PREV_VERSION}):"
+    echo
+    git log --pretty=format:"* %h %s" ${VERSION}...${PREV_VERSION}
+    echo
+) > ${BUILD_DIR}/CHANGELOG
 
 # license file
 cp LICENSE ${BUILD_DIR}/release/LICENSE
