d680139374fcc4ea9e19d0e9e476faf7b2e7f44c
Hang Xie <7977860+hangxie@users.noreply.github.com>
2025-04-10 08:54:00 -0700

    refactor to table-driven unit test

--- internal/io/io_test.go
+++ internal/io/io_test.go
@@ -12,6 +12,7 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
 )
 
@@ -107,72 +108,65 @@ func Test_azureAccessDetail_good_shared_cred(t *testing.T) {
 	require.Equal(t, "*exported.SharedKeyCredential", reflect.TypeOf(cred).String())
 }
 
-func Test_getBucketRegion_s3_non_existent_bucket(t *testing.T) {
-	intVal, _ := rand.Int(rand.Reader, big.NewInt(math.MaxInt64))
-	_, err := getS3Client(fmt.Sprintf("bucket-does-not-exist-%d", intVal.Int64()), true)
-	require.NotNil(t, err)
-	require.Contains(t, err.Error(), "not found")
+func Test_getBucketRegion(t *testing.T) {
+	testCases := map[string]struct {
+		profile string
+		bucket  string
+		public  bool
+		errMsg  string
+	}{
+		"non-existent-bucket":  {"", uuid.New().String(), true, "not found"},
+		"unable-to-get-region": {"", "localhost/something/does/not/matter", true, "unable to get region for S3 bucket"},
+		"bucket-name-with-dot": {"", "xiehang.com", false, ""},
+		"private-bucket":       {"", "doc-example-bucket", true, "S3 bucket doc-example-bucket is not public"},
+		"aws-error":            {"", "00", true, "unrecognized StatusCode from AWS: 400"},
+		"missing-credential":   {uuid.New().String(), "daylight-openstreetmap", false, "failed to get shared config profile"},
+	}
+
+	_ = os.Setenv("AWS_CONFIG_FILE", "/dev/null")
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			_ = os.Setenv("AWS_PROFILE", tc.profile)
+			_, err := getS3Client(tc.bucket, tc.public)
+			if tc.errMsg == "" {
+				require.Nil(t, err)
+			} else {
+				require.NotNil(t, err)
+				require.Contains(t, err.Error(), tc.errMsg)
+			}
+		})
+	}
 }
 
-func Test_getBucketRegion_s3_unable_to_get_region(t *testing.T) {
-	_, err := getS3Client("localhost/something/does/not/matter", true)
-	require.NotNil(t, err)
-	require.Contains(t, err.Error(), "unable to get region for S3 bucket")
-}
+func Test_parseURI(t *testing.T) {
+	testCases := map[string]struct {
+		uri    string
+		scheme string
+		host   string
+		path   string
+		errMsg string
+	}{
+		"invalid-uri":    {"://uri", "", "", "", "unable to parse file location"},
+		"with-user":      {"scheme://username@path/to/file", "scheme", "path", "/to/file", ""},
+		"with-file":      {"file://path/to/file", "file", "", "path/to/file", ""},
+		"with-file-root": {"file:///path/to/file", "file", "", "/path/to/file", ""},
+		"without-file":   {"path/to/file", "file", "", "path/to/file", ""},
+	}
 
-func Test_getBucketRegion_s3_bucket_name_with_dot(t *testing.T) {
-	_, err := getS3Client("xiehang.com", false)
-	require.Nil(t, err)
-}
-
-func Test_getBucketRegion_s3_private_bucket(t *testing.T) {
-	_, err := getS3Client("doc-example-bucket", true)
-	require.NotNil(t, err)
-	require.Contains(t, err.Error(), "S3 bucket doc-example-bucket is not public")
-}
-
-func Test_getBucketRegion_s3_aws_error(t *testing.T) {
-	// AWS bucket name needs to be between 3 and 63 characters
-	// https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
-	_, err := getS3Client("00", true)
-	require.NotNil(t, err)
-	require.Contains(t, err.Error(), "unrecognized StatusCode from AWS: 400")
-}
-
-func Test_getBucketRegion_s3_missing_credential(t *testing.T) {
-	// AWS provides open access: https://registry.opendata.aws/daylight-osm/
-	intVal, _ := rand.Int(rand.Reader, big.NewInt(math.MaxInt64))
-	_ = os.Setenv("AWS_PROFILE", fmt.Sprintf("%d", intVal.Int64()))
-	_, err := getS3Client("daylight-openstreetmap", false)
-	// since aws-go-sdk-v2/config 1.18.45, non-existent profile becomes an error
-	require.NotNil(t, err)
-	require.Contains(t, err.Error(), "failed to get shared config profile")
-}
-
-func Test_parseURI_invalid_uri(t *testing.T) {
-	_, err := parseURI("://uri")
-	require.NotNil(t, err)
-	require.Contains(t, err.Error(), "unable to parse file location")
-}
-
-func Test_parseURI_good(t *testing.T) {
-	u, err := parseURI("scheme://username@path/to/file")
-	require.Nil(t, err)
-	require.Equal(t, "scheme", u.Scheme)
-	require.Equal(t, "path", u.Host)
-	require.Equal(t, "/to/file", u.Path)
-
-	u, err = parseURI("path/to/file")
-	require.Nil(t, err)
-	require.Equal(t, schemeLocal, u.Scheme)
-	require.Equal(t, "", u.Host)
-	require.Equal(t, "path/to/file", u.Path)
-
-	u, err = parseURI("file://path/to/file")
-	require.Nil(t, err)
-	require.Equal(t, schemeLocal, u.Scheme)
-	require.Equal(t, "", u.Host)
-	require.Equal(t, "path/to/file", u.Path)
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			u, err := parseURI(tc.uri)
+			if tc.errMsg != "" {
+				require.NotNil(t, err)
+				require.Contains(t, err.Error(), tc.errMsg)
+				return
+			}
+			require.Nil(t, err)
+			require.Equal(t, tc.scheme, u.Scheme)
+			require.Equal(t, tc.host, u.Host)
+			require.Equal(t, tc.path, u.Path)
+		})
+	}
 }
 
 func Test_NewParquetFileReader_invalid_uri(t *testing.T) {
@@ -183,112 +177,45 @@ func Test_NewParquetFileReader_invalid_uri(t *testing.T) {
 	require.Contains(t, err.Error(), "unable to parse file location")
 }
 
-func Test_NewParquetFileReader_invalid_uri_scheme(t *testing.T) {
-	option := ReadOption{}
-	uri := "invalid-scheme://something"
-	_, err := NewParquetFileReader(uri, option)
-	require.NotNil(t, err)
-	require.Contains(t, err.Error(), "unknown location scheme")
-}
-
-func Test_NewParquetFileReader_local_non_existent_file(t *testing.T) {
-	option := ReadOption{}
-	uri := "file/does/not/exist"
-	_, err := NewParquetFileReader(uri, option)
-	require.NotNil(t, err)
-	require.Contains(t, err.Error(), "no such file or directory")
-}
-
-func Test_NewParquetFileReader_local_not_parquet(t *testing.T) {
-	option := ReadOption{}
-	uri := "../../testdata/not-a-parquet-file"
-	_, err := NewParquetFileReader(uri, option)
-	require.NotNil(t, err)
-	require.Contains(t, err.Error(), "invalid argument")
-}
-
-func Test_NewParquetFileReader_local_good(t *testing.T) {
-	option := ReadOption{}
-	uri := "../../testdata/good.parquet"
-	pr, err := NewParquetFileReader(uri, option)
-	require.Nil(t, err)
-	require.NotNil(t, pr)
-	_ = pr.PFile.Close()
-}
-
-func Test_NewParquetFileReader_s3_non_existent(t *testing.T) {
-	// Make sure there is no AWS access
-	_ = os.Setenv("AWS_CONFIG_FILE", "/dev/null")
-	_ = os.Unsetenv("AWS_PROFILE")
-
-	option := ReadOption{Anonymous: true}
-	intVal, _ := rand.Int(rand.Reader, big.NewInt(math.MaxInt64))
-	uri := fmt.Sprintf("s3://bucket-does-not-exist-%d", intVal.Int64())
-	_, err := NewParquetFileReader(uri, option)
-	require.NotNil(t, err)
-	require.Contains(t, err.Error(), "not found")
-}
-
-func Test_NewParquetFileReader_s3_good(t *testing.T) {
-	// Make sure there is no AWS access
-	_ = os.Setenv("AWS_CONFIG_FILE", "/dev/null")
-	_ = os.Unsetenv("AWS_PROFILE")
-
-	option := ReadOption{Anonymous: true}
-	uri := "s3://daylight-openstreetmap/parquet/osm_features/release=v1.46/type=way/20240506_151445_00143_nanmw_fb5fe2f1-fec8-494f-8c2e-0feb15cedff0"
-	_, err := NewParquetFileReader(uri, option)
-	require.Nil(t, err)
-}
-
-func Test_NewParquetFileReader_s3_non_existent_versioned(t *testing.T) {
-	// Make sure there is no AWS access
-	_ = os.Setenv("AWS_CONFIG_FILE", "/dev/null")
-	_ = os.Unsetenv("AWS_PROFILE")
-
-	option := ReadOption{ObjectVersion: "random-version-id", Anonymous: true}
-	uri := "s3://daylight-openstreetmap/parquet/osm_features/release=v1.46/type=way/20240506_151445_00143_nanmw_fb5fe2f1-fec8-494f-8c2e-0feb15cedff0"
-	_, err := NewParquetFileReader(uri, option)
-	require.NotNil(t, err)
-	// refer to https://docs.aws.amazon.com/AmazonS3/latest/API/API_HeadObject.html
-	// the sample data bucket does not have version enabled so it will return HTTP/400
-	require.Contains(t, err.Error(), "https response error StatusCode: 400")
-}
-
-func Test_NewParquetFileReader_gcs_no_permission(t *testing.T) {
-	// Make sure there is no GCS access
-	_ = os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", "/dev/null")
-
-	option := ReadOption{}
-	uri := "gs://cloud-samples-data/bigquery/us-states/us-states.parquet"
-	_, err := NewParquetFileReader(uri, option)
-	require.NotNil(t, err)
-	require.Contains(t, err.Error(), "failed to create GCS client")
-}
-
-func Test_NewParquetFileReader_gcs_good(t *testing.T) {
-	// Make sure there is no GCS access
-	_ = os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", "/dev/null")
-
-	option := ReadOption{Anonymous: true}
-	uri := "gs://cloud-samples-data/bigquery/us-states/us-states.parquet"
-	_, err := NewParquetFileReader(uri, option)
-	require.Nil(t, err)
-}
-
-func Test_NewParquetFileReader_azblob_no_permission(t *testing.T) {
-	// Use a faked access key so anonymous access will fail
-	randBytes := make([]byte, 64)
-	_, err := rand.Read(randBytes)
-	if err != nil {
-		t.Fatalf("failed to setup test: %s", err.Error())
+func Test_NewParquetFileReader(t *testing.T) {
+	rOpt := ReadOption{}
+	s3URL := "s3://daylight-openstreetmap/parquet/osm_features/release=v1.46/type=way/20240506_151445_00143_nanmw_fb5fe2f1-fec8-494f-8c2e-0feb15cedff0"
+	gcsURL := "gs://cloud-samples-data/bigquery/us-states/us-states.parquet"
+	azblobURL := "wasbs://laborstatisticscontainer@azureopendatastorage.blob.core.windows.net/lfs/part-00000-tid-6312913918496818658-3a88e4f5-ebeb-4691-bfb6-e7bd5d4f2dd0-63558-c000.snappy.parquet"
+	testCases := map[string]struct {
+		uri    string
+		option ReadOption
+		errMsg string
+	}{
+		"invalid-uri":            {"://uri", rOpt, "unable to parse file location"},
+		"invalid-scheme":         {"invalid-scheme://something", rOpt, "unknown location scheme"},
+		"local-file-not-found":   {"file://path/to/file", rOpt, "no such file or directory"},
+		"local-file-not-parquet": {"../../testdata/not-a-parquet-file", rOpt, "invalid argument"},
+		"local-file-good":        {"../../testdata/good.parquet", rOpt, ""},
+		"s3-not-found":           {"s3://bucket-does-not-exist", rOpt, "not found"},
+		"s3-good":                {s3URL, ReadOption{Anonymous: true}, ""},
+		"s3-wrong-version":       {s3URL, ReadOption{ObjectVersion: "random-version-id", Anonymous: true}, "https response error StatusCode: 400"},
+		"gcs-no-permission":      {gcsURL, rOpt, "failed to create GCS client"},
+		"gcs-good":               {gcsURL, ReadOption{Anonymous: true}, ""},
+		"azblob-no-permission":   {azblobURL, rOpt, "failed to open Azure blob object"},
+		"azblob-good":            {azblobURL, ReadOption{Anonymous: true}, ""},
 	}
-	_ = os.Setenv("AZURE_STORAGE_ACCESS_KEY", base64.StdEncoding.EncodeToString(randBytes))
 
-	option := ReadOption{}
-	uri := "wasbs://laborstatisticscontainer@azureopendatastorage.blob.core.windows.net/lfs/part-00000-tid-6312913918496818658-3a88e4f5-ebeb-4691-bfb6-e7bd5d4f2dd0-63558-c000.snappy.parquet"
-	_, err = NewParquetFileReader(uri, option)
-	require.NotNil(t, err)
-	require.Contains(t, err.Error(), "failed to open Azure blob object")
+	_ = os.Setenv("AWS_CONFIG_FILE", "/dev/null")
+	_ = os.Unsetenv("AWS_PROFILE")
+	_ = os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", "/dev/null")
+	_ = os.Setenv("AZURE_STORAGE_ACCESS_KEY", base64.StdEncoding.EncodeToString(uuid.New().NodeID()))
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			_, err := NewParquetFileReader(tc.uri, tc.option)
+			if tc.errMsg == "" {
+				require.Nil(t, err)
+				return
+			}
+			require.NotNil(t, err)
+			require.Contains(t, err.Error(), tc.errMsg)
+		})
+	}
 }
 
 func Test_NewParquetFileWriter_invalid_uri(t *testing.T) {
@@ -490,42 +417,32 @@ func Test_NewJSONWriter_good(t *testing.T) {
 	_ = pw.PFile.Close()
 }
 
-func Test_NewGenericWriter_invalid_uri(t *testing.T) {
-	option := WriteOption{}
-	uri := "://uri"
-	_, err := NewGenericWriter(uri, option, "")
-	require.NotNil(t, err)
-	require.Contains(t, err.Error(), "unable to parse file location")
-}
-
-func Test_NewGenericWriter_invalid_schema(t *testing.T) {
-	tempDir, _ := os.MkdirTemp(os.TempDir(), "split-test")
+func Test_NewGenericWriter_bad(t *testing.T) {
+	tempDir, _ := os.MkdirTemp(os.TempDir(), "generic-writer")
 	defer func() {
 		_ = os.RemoveAll(tempDir)
 	}()
 
-	option := WriteOption{}
-	uri := filepath.Join(tempDir, "json-writer.parquet")
-	_, err := NewGenericWriter(uri, option, "invalid schema")
-	require.NotNil(t, err)
-	require.Contains(t, err.Error(), "error in unmarshalling json schema string:")
+	testCases := map[string]struct {
+		uri    string
+		option WriteOption
+		schema string
+		errMsg string
+	}{
+		"invalud-uri":         {"://uri", WriteOption{}, "", "unable to parse file location"},
+		"schema-not-json":     {filepath.Join(tempDir, "dummy"), WriteOption{}, "invalid schema", "error in unmarshalling json schema string:"},
+		"schema-invalid":      {filepath.Join(tempDir, "dummy"), WriteOption{}, `{"Tag":"name=root","Fields":[{"Tag":"name=id, type=FOOBAR"}]}`, "type FOOBAR: not a valid Type string"},
+		"invalid-compression": {filepath.Join(tempDir, "dummy"), WriteOption{Compression: "FOOBAR"}, `{"Tag":"name=root","Fields":[{"Tag":"name=id, type=INT64"}]}`, "not a valid CompressionCodec string"},
+	}
 
-	_, err = NewGenericWriter(uri, option, `{"Tag":"name=parquet-go-root","Fields":[{"Tag":"name=id, type=FOOBAR"}]}`)
-	require.NotNil(t, err)
-	require.Contains(t, err.Error(), "type FOOBAR: not a valid Type string")
-}
-
-func Test_NewGenericWriter_invalid_compression(t *testing.T) {
-	tempDir, _ := os.MkdirTemp(os.TempDir(), "split-test")
-	defer func() {
-		_ = os.RemoveAll(tempDir)
-	}()
-
-	option := WriteOption{Compression: "dummy"}
-	uri := filepath.Join(tempDir, "json-writer.parquet")
-	_, err := NewGenericWriter(uri, option, `{"Tag":"name=parquet-go-root","Fields":[{"Tag":"name=id, type=INT64"}]}`)
-	require.NotNil(t, err)
-	require.Contains(t, err.Error(), "not a valid CompressionCodec string")
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			pw, err := NewGenericWriter(tc.uri, tc.option, tc.schema)
+			require.NotNil(t, err)
+			require.Nil(t, pw)
+			require.Contains(t, err.Error(), tc.errMsg)
+		})
+	}
 }
 
 func Test_NewGenericWriter_good(t *testing.T) {
