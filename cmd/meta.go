f7a2fb55fa092c69499cd2c17d21522c2a8d47ee
Hang Xie <7977860+hangxie@users.noreply.github.com>
2025-10-27 16:23:46 -0700

    reuse parquet-go function to decode min/max value

--- cmd/meta.go
+++ cmd/meta.go
@@ -2,13 +2,12 @@ package cmd
 
 import (
 	"bytes"
-	"encoding/base64"
-	"encoding/binary"
 	"encoding/json"
 	"fmt"
 	"strings"
 
 	"github.com/hangxie/parquet-go/v2/common"
+	"github.com/hangxie/parquet-go/v2/encoding"
 	"github.com/hangxie/parquet-go/v2/parquet"
 	"github.com/hangxie/parquet-go/v2/types"
 
@@ -223,117 +222,38 @@ func (c MetaCmd) addStatistics(column *columnMeta, statistics *parquet.Statistic
 		return
 	}
 
-	maxValue := c.retrieveValue(statistics.MaxValue, *schemaNode.Type)
-	minValue := c.retrieveValue(statistics.MinValue, *schemaNode.Type)
-	if maxValue != nil {
-		column.MaxValue = decodeMinMaxValue(maxValue, schemaNode)
-	}
-	if minValue != nil {
-		column.MinValue = decodeMinMaxValue(minValue, schemaNode)
-	}
+	precision, scale := int(schemaNode.GetPrecision()), int(schemaNode.GetScale())
+	column.MaxValue = types.ParquetTypeToJSONTypeWithLogical(
+		retrieveValue(statistics.MaxValue, *schemaNode.Type),
+		schemaNode.Type, schemaNode.ConvertedType, schemaNode.LogicalType,
+		precision, scale)
+	column.MinValue = types.ParquetTypeToJSONTypeWithLogical(
+		retrieveValue(statistics.MinValue, *schemaNode.Type),
+		schemaNode.Type, schemaNode.ConvertedType, schemaNode.LogicalType,
+		precision, scale)
 }
 
-func decodeMinMaxValue(value any, schemaNode *pschema.SchemaNode) any {
-	if schemaNode.Type != nil {
-		switch *schemaNode.Type {
-		case parquet.Type_INT96:
-			// INT96 (deprecated) is used for timestamp only
-			return types.INT96ToTime(value.(string))
-		case parquet.Type_FIXED_LEN_BYTE_ARRAY, parquet.Type_BYTE_ARRAY:
-			if schemaNode.ConvertedType == nil && schemaNode.LogicalType == nil {
-				// BYTE_ARRAY and FIXED_LENGTH_BYTE_ARRAY without logical or converted type
-				return base64.StdEncoding.EncodeToString([]byte(value.(string)))
-			}
-		}
-	}
-
-	// backward compatibility
-	if schemaNode.ConvertedType != nil {
-		switch *schemaNode.ConvertedType {
-		case parquet.ConvertedType_INTERVAL:
-			return types.IntervalToString([]byte(value.(string)))
-		case parquet.ConvertedType_DECIMAL:
-			// all sorts of DECIMAL values: INT32, INT64, BYTE_ARRAY
-			return types.ConvertDecimalValue(value, schemaNode.Type, int(*schemaNode.Precision), int(*schemaNode.Scale))
-		case parquet.ConvertedType_DATE:
-			return types.ConvertDateLogicalValue(value)
-		case parquet.ConvertedType_TIME_MICROS, parquet.ConvertedType_TIME_MILLIS:
-			return types.ConvertTimeLogicalValue(value, schemaNode.LogicalType.GetTIME())
-		case parquet.ConvertedType_TIMESTAMP_MICROS, parquet.ConvertedType_TIMESTAMP_MILLIS:
-			return types.ConvertTimestampValue(value, *schemaNode.ConvertedType)
-		case parquet.ConvertedType_BSON:
-			return types.ConvertBSONLogicalValue(value)
-		}
-	}
-
-	if schemaNode.LogicalType != nil {
-		switch {
-		case schemaNode.LogicalType.IsSetDECIMAL():
-			// all sorts of DECIMAL values: INT32, INT64, BYTE_ARRAY
-			return types.ConvertDecimalValue(value, schemaNode.Type, int(*schemaNode.Precision), int(*schemaNode.Scale))
-		case schemaNode.LogicalType.IsSetDATE():
-			return types.ConvertDateLogicalValue(value)
-		case schemaNode.LogicalType.IsSetTIME():
-			return types.ConvertTimeLogicalValue(value, schemaNode.LogicalType.GetTIME())
-		case schemaNode.LogicalType.IsSetTIMESTAMP():
-			if schemaNode.LogicalType.TIMESTAMP.Unit.IsSetMILLIS() {
-				return types.TIMESTAMP_MILLISToISO8601(value.(int64), false)
-			}
-			if schemaNode.LogicalType.TIMESTAMP.Unit.IsSetMICROS() {
-				return types.TIMESTAMP_MICROSToISO8601(value.(int64), false)
-			}
-			return types.TIMESTAMP_NANOSToISO8601(value.(int64), false)
-		case schemaNode.LogicalType.IsSetUUID():
-			return types.ConvertUUIDValue(value)
-		case schemaNode.LogicalType.IsSetBSON():
-			return types.ConvertBSONLogicalValue(value)
-		case schemaNode.LogicalType.IsSetFLOAT16():
-			return types.ConvertFloat16LogicalValue(value)
-		}
-	}
-
-	return value
-}
-
-func (c MetaCmd) retrieveValue(value []byte, parquetType parquet.Type) any {
+func retrieveValue(value []byte, parquetType parquet.Type) any {
 	if value == nil {
 		return nil
 	}
 
-	buf := bytes.NewReader(value)
-	switch parquetType {
-	case parquet.Type_BOOLEAN:
-		var b bool
-		if err := binary.Read(buf, binary.LittleEndian, &b); err != nil {
-			return "failed to read data as BOOLEAN"
-		}
-		return b
-	case parquet.Type_INT32:
-		var i32 int32
-		if err := binary.Read(buf, binary.LittleEndian, &i32); err != nil {
-			return "failed to read data as INT32"
-		}
-		return i32
-	case parquet.Type_INT64:
-		var i64 int64
-		if err := binary.Read(buf, binary.LittleEndian, &i64); err != nil {
-			return "failed to read data as INT64"
-		}
-		return i64
-	case parquet.Type_FLOAT:
-		var f32 float32
-		if err := binary.Read(buf, binary.LittleEndian, &f32); err != nil {
-			return "failed to read data as FLOAT"
-		}
-		return f32
-	case parquet.Type_DOUBLE:
-		var f64 float64
-		if err := binary.Read(buf, binary.LittleEndian, &f64); err != nil {
-			return "failed to read data as DOUBLE"
-		}
-		return f64
+	// Statistics for BYTE_ARRAY and FIXED_LEN_BYTE_ARRAY types don't include
+	// the length prefix that PLAIN encoding normally has, so we handle them directly
+	if parquetType == parquet.Type_BYTE_ARRAY || parquetType == parquet.Type_FIXED_LEN_BYTE_ARRAY {
+		return string(value)
 	}
-	return string(value)
+
+	// For numeric types, use parquet-go's encoding functions
+	buf := bytes.NewReader(value)
+	vals, err := encoding.ReadPlain(buf, parquetType, 1, 0)
+	if err != nil {
+		return fmt.Sprintf("failed to read data as %s: %v", parquetType.String(), err)
+	}
+	if len(vals) == 0 {
+		return nil
+	}
+	return vals[0]
 }
 
 func encodingToString(encodings []parquet.Encoding) []string {
