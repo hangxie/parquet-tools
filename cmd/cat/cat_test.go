2297fc8f084bd75fcc63e7bd657fd6837428deac
Hang Xie <7977860+hangxie@users.noreply.github.com>
2026-01-31 21:02:54 -0800

    replace panic with Fatal in tests

--- cmd/cat/cat_test.go
+++ cmd/cat/cat_test.go
@@ -226,7 +226,7 @@ func BenchmarkCatCmd(b *testing.B) {
 	// savedStdout, savedStderr := os.Stdout, os.Stderr
 	devNull, err := os.OpenFile(os.DevNull, os.O_WRONLY, 0o666)
 	if err != nil {
-		panic(err)
+		b.Fatal(err)
 	}
 	os.Stdout = devNull
 	defer func() {
